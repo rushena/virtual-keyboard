91b8fc991dbeb6e8a694882d93ac3f6f4114f81f
Rushen Adjigeldieva <rushena2706@gmail.com>
2023-05-01 03:32:34 +0500

    feat: add keyboard data

---
+++ src/data.js
@@ -0,0 +1,601 @@
+export default [
+	{
+		id: 1,
+		firstValue: {
+			lang1: '`',
+			lang2: '`'
+		},
+		secondValue: {
+			lang1: '~',
+			lang2: '~'
+		}
+	},
+	{
+		id: 2,
+		firstValue: {
+			lang1: '1',
+			lang2: '1'
+		},
+		secondValue: {
+			lang1: '!',
+			lang2: '!'
+		}
+	},
+	{
+		id: 3,
+		firstValue: {
+			lang1: '2',
+			lang2: '2'
+		},
+		secondValue: {
+			lang1: '@',
+			lang2: '"'
+		}
+	},
+	{
+		id: 4,
+		firstValue: {
+			lang1: '3',
+			lang2: '3'
+		},
+		secondValue: {
+			lang1: '#',
+			lang2: '№'
+		}
+	},
+	{
+		id: 5,
+		firstValue: {
+			lang1: '4',
+			lang2: '4'
+		},
+		secondValue: {
+			lang1: '$',
+			lang2: ';'
+		}
+	},
+	{
+		id: 6,
+		firstValue: {
+			lang1: '5',
+			lang2: '5'
+		},
+		secondValue: {
+			lang1: '%',
+			lang2: '%'
+		}
+	},
+	{
+		id: 7,
+		firstValue: {
+			lang1: '6',
+			lang2: '6'
+		},
+		secondValue: {
+			lang1: '^',
+			lang2: ':'
+		}
+	},
+	{
+		id: 8,
+		firstValue: {
+			lang1: '7',
+			lang2: '7'
+		},
+		secondValue: {
+			lang1: '&',
+			lang2: '?'
+		}
+	},
+	{
+		id: 9,
+		firstValue: {
+			lang1: '8',
+			lang2: '8'
+		},
+		secondValue: {
+			lang1: '*',
+			lang2: '*'
+		}
+	},
+	{
+		id: 10,
+		firstValue: {
+			lang1: '9',
+			lang2: '9'
+		},
+		secondValue: {
+			lang1: '(',
+			lang2: '('
+		}
+	},
+	{
+		id: 11,
+		firstValue: {
+			lang1: '0',
+			lang2: '0'
+		},
+		secondValue: {
+			lang1: ')',
+			lang2: ')'
+		}
+	},
+	{
+		id: 12,
+		firstValue: {
+			lang1: '-',
+			lang2: '-'
+		},
+		secondValue: {
+			lang1: '_',
+			lang2: '_'
+		}
+	},
+	{
+		id: 13,
+		firstValue: {
+			lang1: '=',
+			lang2: '='
+		},
+		secondValue: {
+			lang1: '+',
+			lang2: '+'
+		}
+	},
+	{
+		id: 14,
+		firstValue: {
+			lang1: 'Backspace',
+			lang2: 'Backspace'
+		},
+		secondValue: null
+	},
+	{
+		id: 15,
+		firstValue: {
+			lang1: 'Tab',
+			lang2: 'Tab'
+		},
+		secondValue: null
+	},
+	{
+		id: 16,
+		firstValue: {
+			lang1: 'Q',
+			lang2: 'Й'
+		},
+		secondValue: null
+	},
+	{
+		id: 17,
+		firstValue: {
+			lang1: 'W',
+			lang2: 'Ц'
+		},
+		secondValue: null
+	},
+	{
+		id: 18,
+		firstValue: {
+			lang1: 'E',
+			lang2: 'У'
+		},
+		secondValue: null
+	},
+	{
+		id: 19,
+		firstValue: {
+			lang1: 'R',
+			lang2: 'К'
+		},
+		secondValue: null
+	},
+	{
+		id: 20,
+		firstValue: {
+			lang1: 'T',
+			lang2: 'Е'
+		},
+		secondValue: null
+	},
+	{
+		id: 21,
+		firstValue: {
+			lang1: 'Y',
+			lang2: 'Н'
+		},
+		secondValue: null
+	},
+	{
+		id: 22,
+		firstValue: {
+			lang1: 'U',
+			lang2: 'Г'
+		},
+		secondValue: null
+	},
+	{
+		id: 23,
+		firstValue: {
+			lang1: 'I',
+			lang2: 'Ш'
+		},
+		secondValue: null
+	},
+	{
+		id: 24,
+		firstValue: {
+			lang1: 'O',
+			lang2: 'Щ'
+		},
+		secondValue: null
+	},
+	{
+		id: 25,
+		firstValue: {
+			lang1: 'P',
+			lang2: 'З'
+		},
+		secondValue: null
+	},
+	{
+		id: 26,
+		firstValue: {
+			lang1: '[',
+			lang2: 'Х'
+		},
+		secondValue: {
+			lang1: '{',
+			lang2: ''
+		}
+	},
+	{
+		id: 27,
+		firstValue: {
+			lang1: ']',
+			lang2: 'Ъ'
+		},
+		secondValue: {
+			lang1: '}',
+			lang2: ''
+		}
+	},
+	{
+		id: 28,
+		firstValue: {
+			lang1: 'Delete',
+			lang2: 'Delete'
+		},
+		secondValue: null
+	},
+	{
+		id: 29,
+		firstValue: {
+			lang1: 'Caps Lock',
+			lang2: 'Caps Lock'
+		},
+		secondValue: null
+	},
+	{
+		id: 30,
+		firstValue: {
+			lang1: 'A',
+			lang2: ''
+		},
+		secondValue: null
+	},
+	{
+		id: 31,
+		firstValue: {
+			lang1: 'S',
+			lang2: 'Ы'
+		},
+		secondValue: null
+	},
+	{
+		id: 32,
+		firstValue: {
+			lang1: 'D',
+			lang2: 'В'
+		},
+		secondValue: null
+	},
+	{
+		id: 33,
+		firstValue: {
+			lang1: 'F',
+			lang2: 'А'
+		},
+		secondValue: null
+	},
+	{
+		id: 34,
+		firstValue: {
+			lang1: 'G',
+			lang2: 'П'
+		},
+		secondValue: null
+	},
+	{
+		id: 35,
+		firstValue: {
+			lang1: 'H',
+			lang2: 'Р'
+		},
+		secondValue: null
+	},
+	{
+		id: 36,
+		firstValue: {
+			lang1: 'J',
+			lang2: 'О'
+		},
+		secondValue: null
+	},
+	{
+		id: 37,
+		firstValue: {
+			lang1: 'K',
+			lang2: 'Л'
+		},
+		secondValue: null
+	},
+	{
+		id: 38,
+		firstValue: {
+			lang1: 'L',
+			lang2: 'Д'
+		},
+		secondValue: null
+	},
+	{
+		id: 39,
+		firstValue: {
+			lang1: ';',
+			lang2: 'Ж'
+		},
+		secondValue: {
+			lang1: ':',
+			lang2: ''
+		}
+	},
+	{
+		id: 40,
+		firstValue: {
+			lang1: '\'',
+			lang2: 'Э'
+		},
+		secondValue: {
+			lang1: '"',
+			lang2: ''
+		}
+	},
+	{
+		id: 41,
+		firstValue: {
+			lang1: '\\',
+			lang2: '/'
+		},
+		secondValue: {
+			lang1: '|',
+			lang2: '/'
+		}
+	},
+	{
+		id: 42,
+		firstValue: {
+			lang1: 'Enter',
+			lang2: 'Enter'
+		},
+		secondValue: null
+	},
+	{
+		id: 43,
+		firstValue: {
+			lang1: 'Shift',
+			lang2: 'Shift'
+		},
+		secondValue: null
+	},
+	{
+		id: 44,
+		firstValue: {
+			lang1: 'Z',
+			lang2: 'Я'
+		},
+		secondValue: null
+	},
+	{
+		id: 45,
+		firstValue: {
+			lang1: 'X',
+			lang2: 'Ч'
+		},
+		secondValue: null
+	},
+	{
+		id: 46,
+		firstValue: {
+			lang1: 'C',
+			lang2: 'С'
+		},
+		secondValue: null
+	},
+	{
+		id: 47,
+		firstValue: {
+			lang1: 'V',
+			lang2: 'М'
+		},
+		secondValue: null
+	},
+	{
+		id: 48,
+		firstValue: {
+			lang1: 'B',
+			lang2: 'И'
+		},
+		secondValue: null
+	},
+	{
+		id: 49,
+		firstValue: {
+			lang1: 'N',
+			lang2: 'Т'
+		},
+		secondValue: null
+	},
+	{
+		id: 50,
+		firstValue: {
+			lang1: 'M',
+			lang2: 'Ь'
+		},
+		secondValue: null
+	},
+	{
+		id: 51,
+		firstValue: {
+			lang1: ',',
+			lang2: 'Б'
+		},
+		secondValue: {
+			lang1: '<',
+			lang2: ''
+		}
+	},
+	{
+		id: 52,
+		firstValue: {
+			lang1: '.',
+			lang2: 'Ю'
+		},
+		secondValue: {
+			lang1: '>',
+			lang2: ''
+		}
+	},
+	{
+		id: 53,
+		firstValue: {
+			lang1: '/',
+			lang2: '.'
+		},
+		secondValue: {
+			lang1: '?',
+			lang2: ','
+		}
+	},
+	{
+		id: 54,
+		firstValue: {
+			lang1: 'Shift',
+			lang2: 'Shift'
+		},
+		secondValue: null
+	},
+	{
+		id: 55,
+		firstValue: {
+			lang1: '↑',
+			lang2: '↑'
+		},
+		secondValue: null
+	},
+	{
+		id: 56,
+		firstValue: {
+			lang1: 'En',
+			lang2: 'Ру'
+		},
+		secondValue: null
+	},
+	{
+		id: 57,
+		firstValue: {
+			lang1: 'Ctrl',
+			lang2: 'Ctrl'
+		},
+		secondValue: null
+	},
+	{
+		id: 58,
+		firstValue: {
+			lang1: 'Win',
+			lang2: 'Win'
+		},
+		secondValue: null
+	},
+	{
+		id: 59,
+		firstValue: {
+			lang1: 'Alt',
+			lang2: 'Alt'
+		},
+		secondValue: null
+	},
+	{
+		id: 60,
+		firstValue: {
+			lang1: 'Space',
+			lang2: 'Пробел'
+		},
+		secondValue: null
+	},
+	{
+		id: 61,
+		firstValue: {
+			lang1: 'Alt',
+			lang2: 'Alt'
+		},
+		secondValue: null
+	},
+	{
+		id: 62,
+		firstValue: {
+			lang1: 'Win',
+			lang2: 'Win'
+		},
+		secondValue: null
+	},
+	{
+		id: 63,
+		firstValue: {
+			lang1: '≣',
+			lang2: '≣'
+		},
+		secondValue: null
+	},
+	{
+		id: 64,
+		firstValue: {
+			lang1: 'Ctrl',
+			lang2: 'Ctrl'
+		},
+		secondValue: null
+	},
+	{
+		id: 65,
+		firstValue: {
+			lang1: '←',
+			lang2: '←'
+		},
+		secondValue: null
+	},
+	{
+		id: 66,
+		firstValue: {
+			lang1: '↓',
+			lang2: '↓'
+		},
+		secondValue: null
+	},
+	{
+		id: 67,
+		firstValue: {
+			lang1: '→',
+			lang2: '→'
+		},
+		secondValue: null
+	}
+]
